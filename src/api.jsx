a74c34d7181a7f0a9a670a16c36c599a79ffe30e
Arturo Aliaga <jaav2801@gmail.com>
2024-10-03 23:20:47 -0400

    mas practicas

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backendfitmrp-production.up.railway.app/api',  // Cambia esto a la URL de tu backend
+  baseURL: 'https://backendfitmrp-production.up.railway.app/api',  // URL de tu backend
+  withCredentials: false,  // Permite solicitudes sin credenciales
 });
 
 export default api;
